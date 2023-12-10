24e03d01990937b7286aed056847381b86bb1665
DragonDare <deykohav@gmail.com>
2023-12-10 08:36:53 +0530

    Smart contracts for deploying initial distribution added

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,33 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import 'dotenv/config';
-
-const { PRIVATE_KEY } = process.env;
 
 const config: HardhatUserConfig = {
-  // defaultNetwork: "PolygonMumbai",
-  // networks: {
-  //   hardhat: {
-  //     // You can leave this empty for the default Hardhat network setup
-  //   },
-  //   PolygonMumbai: {
-  //     url: "https://rpc-mumbai.maticvigil.com",
-  //     accounts: [PRIVATE_KEY as string], // Adjust the type assertion based on your use case
-  //   },
-  // },
-  defaultNetwork: "FileCoinCalibration",
+  defaultNetwork: "PolygonMumbai",
   networks: {
     hardhat: {
       // You can leave this empty for the default Hardhat network setup
     },
-    FileCoinCalibration: {
-      url: "https://api.calibration.node.glif.io/rpc/v1",
+    PolygonMumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
       accounts: [PRIVATE_KEY as string], // Adjust the type assertion based on your use case
     },
-    PolygonMumbai: {
-          url: "https://rpc-mumbai.maticvigil.com",
-          accounts: [PRIVATE_KEY as string], // Adjust the type assertion based on your use case
-        },
   },
   solidity: {
     version: "0.8.20",
